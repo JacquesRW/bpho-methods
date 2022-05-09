6b69bd1dca6a01e5cb0d71a0a7f5e8727408cd99
Jacques <jamie.whiting2003@gmail.com>
2022-05-09 17:38:59 +0100

    improved header file

--- euler/dll/euler_scheme.h
+++ euler/dll/euler_scheme.h
@@ -1,3 +1,17 @@
-#define IMPORT_API __declspec(dllimport)
+#pragma once
 
-extern "C" IMPORT_API double** euler_scheme(double dh, double h0, double h1, double P0, double T0, double U);
+#if defined(_MSC_VER)
+    //  Microsoft 
+    #define EXPORT __declspec(dllexport)
+    #define IMPORT __declspec(dllimport)
+#elif defined(__GNUC__)
+    //  GCC
+    #define EXPORT __attribute__((visibility("default")))
+    #define IMPORT
+#else
+    #define EXPORT
+    #define IMPORT
+    #pragma warning Unknown dynamic link import/export semantics.
+#endif
+
+extern "C" IMPORT double** euler_scheme(double dh, double h0, double h1, double P0, double T0, double U);
